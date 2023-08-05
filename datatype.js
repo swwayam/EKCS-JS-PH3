056fa201a5a29267dc1f7cfd32c1566507449932
swwayam <codingswayam24@gmail.com>
2023-08-05 13:28:56 +0530

    EKCS-PH-3 JS Start

---
+++ datatype.js
@@ -0,0 +1,34 @@
+// const whoAmI = "I am a developer"
+// const job = "I work on ADOBE" 
+
+// console.log(whoAmI + job)
+
+// const num1 = 10
+// const num2 = 20
+
+// console.log(num1 + num2);
+
+// Data types
+
+const name = "Pankaj" // text -> string
+const num = 987232323 // number -> number | integer (bina decimal wala)
+const percent = 99.99 // number -> number | float (point wala number)
+const tAndF = true    // boolean -> matlab true ya false value
+
+// Examples 
+const num1 = 10 // :number
+const num2 = 20 // :number
+
+const firstName = "Pankaj " // :string
+const lastName = "Singh" // :string
+
+const marks = 99.99 // :number
+
+const bool = true // :boolean 
+const bool1 = false // :boolean
+
+console.log(num1 + num2); // 30
+console.log(firstName + lastName); // Pankaj Singh
+console.log(marks + num1) // 109.99 :number // float is greater than integer
+console.log(firstName + num1); // Pankaj 10 // string + int -> string
+console.log(bool + bool1); // true // boolean
---
+++ var.js
@@ -0,0 +1,30 @@
+// This is single line comment
+
+/* 
+   This is multiline comment
+*/
+
+// Variables In JS 
+// Example - pankaj = 10 | (pankaj + pankaj) * pankaj || BODMAS
+
+// Variables using let | Changable Pankaj
+
+let pankaj = 10
+console.log(pankaj)
+
+let manoj = pankaj + 100
+console.log(pankaj)
+console.log("I am manoj " + manoj);
+
+// Variables using const | Permanat Manish
+
+const manish = 20
+console.log("Hi i am manish and my value is " + pankaj);
+
+const swayam = manish + 20
+console.log(swayam);
+
+
+
+
+
